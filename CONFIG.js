8e32c2c4f240fa36ad5e30badb7d8d26ab37a925
hskyiutskygacnhieuu <104723325+hskyiutskygacnhieuu@users.noreply.github.com>
2022-05-01 20:48:31 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Anh ơi, anh làm người yêu bé nhaaa",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "Chịu thì chịu hong chịu thì chịu. Embe iu anh nhiều nhắm",
+  buttonYes: "Anh đồng ý. Anh yêu béeee",
+  buttonNo: "Hong đâu, liuliu",
+  titleModar: "ỏooo, thiệt hỏ",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Hihi, yêu anh siu nhiều luôn </br> Mãi iu nhóoo </br> "Hạnh iu Nguyễn Hà Thành </br>  Moah moah "',
 };
